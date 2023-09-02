70278a9834ab71597b74b14879b52b50904a9a09
NMishchenko <mishchenko.nls@gmail.com>
2023-09-03 01:25:19 +0300

    Add constants service, register it with useValue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { ConstantsModel } from './app/core/models/constants.model';
+import { ConstantsServiceToken } from './app/core/services/constants.service';
 
+const defaultConstantsModel: ConstantsModel = {
+  App: "Sus Shop",
+  Version: "1.2.0",
+  API_URL: "https://sus-shop.epam.ua"
+}
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
+    providers: [
+      importProvidersFrom(BrowserModule),
+      { provide: ConstantsServiceToken, useValue: defaultConstantsModel }
+    ]
 })
   .catch(err => console.error(err));
